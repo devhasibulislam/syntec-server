b9bcfc0a7d64c6564d5572108c1f573f8d840860
hasibulislam999 <hasib143sl@gmail.com>
2022-09-29 13:52:12 +0600

    service controller modified through patch method

--- controllers/service.controller.js
+++ controllers/service.controller.js
@@ -1,5 +1,8 @@
 /* internal import */
-const { postAServiceToService } = require("../services/service.service");
+const {
+  postAServiceToService,
+  updateAServiceFromService,
+} = require("../services/service.service");
 
 /* insert service to db */
 exports.postAServiceToController = async (req, res, next) => {
@@ -18,17 +21,33 @@ exports.postAServiceToController = async (req, res, next) => {
 };
 
 /* display services from db */
-exports.displayAllServicesFromController = async(req, res, next) => {
-    try {
-        const result = await displayAllServicesFromService();
+exports.displayAllServicesFromController = async (req, res, next) => {
+  try {
+    const result = await displayAllServicesFromService();
 
-        res.status(200).json({
-            acknowledgement: true,
-            message: "OK",
-            description: "Fetching all services from DB",
-            data: result
-        })
-    } catch (error) {
-        next(error);
-    }
-}
+    res.status(200).json({
+      acknowledgement: true,
+      message: "OK",
+      description: "Fetching all services from DB",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/* update specific service from db */
+exports.updateAServiceFromController = async (req, res, next) => {
+  try {
+    const result = await updateAServiceFromService(req.params.id, req.body);
+
+    res.status(202).json({
+      acknowledgement: true,
+      message: "Accepted",
+      description: "Specified data updated to DB",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
