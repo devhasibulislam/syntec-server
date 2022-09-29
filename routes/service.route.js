54a8eae5fc6418ab005dd8d14445808d0fef8401
hasibulislam999 <hasib143sl@gmail.com>
2022-09-29 13:40:33 +0600

    service router modified

--- routes/service.route.js
+++ routes/service.route.js
@@ -2,10 +2,15 @@
 const express = require("express");
 
 /* internal import */
+const {
+  postAServiceToController,
+} = require("../controllers/service.controller");
+
+/* internal import */
 
 /* router level connection */
 const router = express.Router();
 
-router.route("/").get().post();
+router.route("/").get(postAServiceToController).post();
 
 module.exports = router;
