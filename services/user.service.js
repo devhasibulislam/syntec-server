499b188d7b2e3e102159e87d6cc2850088ca45a9
hasibulislam999 <hasib143sl@gmail.com>
2022-09-29 13:17:33 +0600

    user services added

---
+++ services/user.service.js
@@ -0,0 +1,27 @@
+/* internal imports */
+const User = require("../schemas/user.schema");
+const bcrypt = require("bcryptjs");
+
+/* signed up to db */
+exports.postAnUserToServiceForRegister = async (data) => {
+  const user = new User(data);
+  const result = await user.save();
+
+  return result;
+};
+
+/* signed in to db */
+exports.postAnUserToServiceForLogin = async (data) => {
+  const user = await User.findOne({ email: data.email });
+
+  if (user) {
+    const isValidPassword = bcrypt.compareSync(data.password, user.password);
+    if (isValidPassword) {
+      console.log("Password is correct!");
+    } else {
+      console.log("Password is wrong!");
+    }
+  } else {
+    console.log("User not exist!");
+  }
+};
